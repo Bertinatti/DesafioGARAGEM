6ce2d69ea72ef4731d08f15030cc721d93bb5214
Leonardo C. Bertinatti <102967582+Bertinatti@users.noreply.github.com>
2022-06-17 14:45:40 -0300

    Desafio GARAGEM - concluído

---
+++ DesafioGaragem/Persistencia.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioGaragem
+{
+    internal class Persistencia
+    {
+        /// <summary>
+        /// Método para ler os dados persistidos no arquivo, com o histórico de todos os veículos que estão usando o sistema.
+        /// </summary>
+        /// <param name="lista">Lista com os veículos que estão usando o sistema.</param>
+        public static void lerArquivoVeiculosEntrada(List<Veiculo> lista)
+        {
+            string nomeArquivo = "veiculosEntrada.dat";
+
+            try
+            {
+                StreamReader leitor = new StreamReader(nomeArquivo);
+
+                string linha;
+                string[] linhaSplit;
+
+                do
+                {
+                    linha = leitor.ReadLine();
+
+                    if (linha != null && linha != "")
+                    {
+                        linhaSplit = linha.Split(';');
+
+                        Veiculo veiculo = new Veiculo(linhaSplit[0], linhaSplit[1], linhaSplit[2]);
+
+                        lista.Add(veiculo);
+
+                    }
+
+                } while (!leitor.EndOfStream);
+
+                leitor.Close();
+
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro ao ler arquivo: " + e.Message);
+            }
+
+            StreamWriter escritor = new StreamWriter(nomeArquivo, true);
+            escritor.Close();
+        }
+        /// <summary>
+        /// Método para ler os dados persistidos no arquivo, com o histórico de todos os veículos que utilizaram o sistema.
+        /// </summary>
+        /// <param name="lista">Lista com os veículos que usaram o sistema.</param>
+        public static void lerArquivoVeiculosSaida(List<Veiculo> lista)
+        {
+            string nomeArquivo = "veiculosSaida.dat";
+
+            try
+            {
+                StreamReader leitor = new StreamReader(nomeArquivo);
+
+                string linha;
+                string[] linhaSplit;
+
+                do
+                {
+                    linha = leitor.ReadLine();
+
+                    if (linha != null && linha != "")
+                    {
+                        linhaSplit = linha.Split(';');
+
+                        Veiculo veiculo = new Veiculo(linhaSplit[0],linhaSplit[1], linhaSplit[2], int.Parse(linhaSplit[3]), double.Parse(linhaSplit[4]));
+
+                        lista.Add(veiculo);
+
+                    }
+
+                } while (!leitor.EndOfStream);
+
+                leitor.Close();
+
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro ao ler arquivo: " + e.Message);
+            }
+
+            StreamWriter escritor = new StreamWriter(nomeArquivo, true);
+            escritor.Close();
+        }
+        /// <summary>
+        /// Método para ler os dados persistidos no arquivo, com o preço cobrado e o número de vagas da garagem.
+        /// </summary>
+        /// <param name="lista">Lista que irá guardar o preço cobrado e o número de vagas.</param>
+        public static void lerArquivoConfiguracaoGaragem(List<Garagem> lista)
+        {
+            string nomeArquivo = "configuracaoGaragem.dat";
+
+            try
+            {
+                StreamReader leitor = new StreamReader(nomeArquivo);
+
+                string linha;
+                string[] linhaSplit;
+                                
+                linha = leitor.ReadLine();
+
+                if (linha != null && linha != "")
+                {
+                    linhaSplit = linha.Split(';');
+                    Garagem garagem = new Garagem(int.Parse(linhaSplit[0]),double.Parse(linhaSplit[1]));
+                    lista.Clear();
+                    lista.Add(garagem);
+                }
+                leitor.Close();
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro ao ler arquivo: " + e.Message);
+            }
+
+            StreamWriter escritor = new StreamWriter(nomeArquivo, true);
+            escritor.Close();
+        }
+        /// <summary>
+        /// Método para persistir os dados no arquivo de veículos que estão utilizando o sistema.
+        /// </summary>
+        /// <param name="veiculo">Objeto da classe veículo com os dados de entrada</param>
+        public static void gravarArquivoVeiculosEntrada(Veiculo veiculo)
+        {
+            string nomeArquivo = "veiculosEntrada.dat";
+
+            try
+            {
+                StreamWriter escritor = new StreamWriter(nomeArquivo, true);
+                escritor.WriteLine(veiculo.PlacaVeiculo + ";" + veiculo.DataEntrada + ";" + veiculo.HoraEntrada);
+                escritor.Flush();
+                escritor.Close();
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro ao gravar o arquivo: " + e.Message);
+            }
+
+        }
+        /// <summary>
+        /// Método para persistir os dados no arquivo de veículos que utilizaram o sistema.
+        /// </summary>
+        /// <param name="veiculo">Objeto da classe veículo com os dados de saída.</param>
+        public static void gravarArquivoVeiculosSaida(Veiculo veiculo)
+        {
+            string nomeArquivo = "veiculosSaida.dat";
+
+            try
+            {
+                StreamWriter escritor = new StreamWriter(nomeArquivo, true);
+                escritor.WriteLine(veiculo.PlacaVeiculo + ";" + veiculo.DataEntrada + ";" + veiculo.HoraEntrada + ";" + veiculo.TempoPermanencia + ";" + veiculo.ValorCobrado);
+                escritor.Flush();
+                escritor.Close();
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro ao gravar o arquivo: " + e.Message);
+            }
+
+        }
+        /// <summary>
+        /// Método para persistir os dados no arquivo de configuração da garagem.
+        /// </summary>
+        /// <param name="garagem">Recebe o objeto com os dados da garagem.</param>
+        public static void gravarArquivoConfiguracaoGaragem(Garagem garagem)
+        {
+            string nomeArquivo = "configuracaoGaragem.dat";
+
+            try
+            {
+                StreamWriter escritor = new StreamWriter(nomeArquivo);
+                escritor.WriteLine(garagem.Vagas + ";" + garagem.PrecoCobrado);
+                escritor.Flush();
+                escritor.Close();
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro ao gravar o arquivo: " + e.Message);
+            }
+
+        }
+        /// <summary>
+        /// Método estático para limpar o arquivo de entrada quando o sistema se encontrar vazio.
+        /// </summary>
+        public static void limparArquivoVeiculosEntrada()
+        {
+            string nomeArquivo = "veiculosEntrada.dat";
+
+            try
+            {
+                StreamWriter escritor = new StreamWriter(nomeArquivo);
+                escritor.WriteLine("");
+                escritor.Flush();
+                escritor.Close();
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro ao gravar o arquivo: " + e.Message);
+            }
+
+        }
+    }
+}
---
+++ DesafioGaragem/frmConfiguracaoGaragem.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesafioGaragem
+{
+    public partial class frmConfiguracaoGaragem : Form
+    {
+        /// <summary>
+        /// Método para popular as Label que mostram o preço cobrado e o número de vagas.
+        /// </summary>
+        /// <param name="lista">Lista que guarda os valores de configuração da garagem.</param>
+        private void popularLabel(List<Garagem> lista)
+        {
+            Persistencia.lerArquivoConfiguracaoGaragem(lista);
+            foreach (Garagem g in lista)
+            {
+                lblNumeroVagas.Text = Convert.ToString(g.Vagas);
+                lblValorAtual.Text = g.PrecoCobrado.ToString("F");
+            }
+        }
+        public frmConfiguracaoGaragem()
+        {
+            InitializeComponent();
+            Persistencia.lerArquivoConfiguracaoGaragem(listaGaragem);
+            popularLabel(listaGaragem);
+        }
+        /// <summary>
+        /// Método para limpar os campos do Forms.
+        /// </summary>
+        private void limpaCampos()
+        {
+            tbNovoPreco.Text = "";
+            tbVagasGaragem.Text = "";
+        }
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            frmPrincipal frmPrincipal = new frmPrincipal();
+            frmPrincipal.ShowDialog();
+        }
+        private void frmConfiguracaoGaragem_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Dispose();
+        }
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            limpaCampos();
+        }
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+            if(tbNovoPreco.Text == "" || tbVagasGaragem.Text == "")
+            {
+                MessageBox.Show("Digite os campos corretamente.", "Erro - CAMPOS EM BRANCO.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                Persistencia.gravarArquivoConfiguracaoGaragem(new Garagem(int.Parse(tbVagasGaragem.Text), double.Parse(tbNovoPreco.Text)));
+                MessageBox.Show("Dados cadastrados com sucesso.", "Cadastro de configuração - GARAGEM.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                popularLabel(listaGaragem);
+                limpaCampos();
+            }
+        }
+
+        List<Garagem> listaGaragem = new List<Garagem>();
+
+    }
+}
